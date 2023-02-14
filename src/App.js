73ec7a087366ffe7b2ba4d2238fcbb272385c423
Matti Räty <mattiraty@hotmail.com>
2023-02-14 13:56:19 +0200

    created addTask() method

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>TO-DO App</h1>
-        <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-        </ul>  
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tasks: []
+    }
+
+    this.addTask = this.addTask.bind(this);
+  }
+
+  addTask(task) {
+    let tasks = this.state.tasks;
+
+    tasks.push(task);
+
+    this.setState({ tasks: tasks })
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <h1>TO-DO App</h1>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>  
+        
+      </div>
+    );
+  }
+  
 }
 
 export default App;
